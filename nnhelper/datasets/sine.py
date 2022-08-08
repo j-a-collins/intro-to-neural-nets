64eb880880423236b2d8084c79f83b8b6a3f190c
J A Collins <45298131+j-a-collins@users.noreply.github.com>
2022-08-08 10:12:43 +0100

    Delete nnhelper directory

--- nnhelper/datasets/sine.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def create_data(samples=1000):
-    """Creates a sine sample dataset"""
-    x = np.arange(samples).reshape(-1, 1) / samples
-    y = np.sin(2 * np.pi * x).reshape(-1, 1)
-
-    return x, y
